44da45f37eccc3aed969fb79bc2a1b9dcbe51c71
liwanlin41 <liwanlin@mit.edu>
2020-01-09 18:37:08 -0500

    screwing around pt 2

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "home page"
+    return render_template('index.html')
+#    return "home page"
+
+@app.route('/clubhouse')
+def club_home():
+    return render_template('/clubhouse/home.html')

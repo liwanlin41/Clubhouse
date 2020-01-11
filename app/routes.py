47ecacc5e19f2ffdefaa6b11d1113172d2c4edcc
liwanlin41 <liwanlin@mit.edu>
2020-01-11 14:54:26 -0500

    image testing

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,8 @@ from flask import render_template, flash, redirect, request
 from app import app
 from app.forms import LoginForm
 
+### homepages
+
 @app.route('/')
 def home():
 #    return "Hello"
@@ -14,11 +16,25 @@ def home():
 def club_home():
     return render_template('/clubhouse/home.html')
 
-# basic login form, I don't think it works yet
+@app.route('/admin')
+def admin_home():
+    return render_template('/admin/home.html')
+
+# basic login form, it doesn't post anything yet
 @app.route('/clubhouse/login', methods=['GET','POST'])
-def login():
+def coord_login():
     form = LoginForm()
     if form.validate_on_submit():
         raw_data = request.form # read user input to form
+        # TODO: check if username & password are valid
         return redirect('/clubhouse')
+    return render_template('login.html', form=form)
+
+# same as above, only for admins
+@app.route('/admin/login', methods=['GET','POST'])
+def admin_login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        raw_data = request.form
+        return redirect('admin')
     return render_template('login.html', form=form)

65278ca9550dd0fb584b9fb23a3a0974a3e112cf
liwanlin41 <liwanlin@mit.edu>
2020-01-14 23:02:59 -0500

    include database sketch?

--- app/forms.py
+++ app/forms.py
@@ -5,6 +5,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, Selec
 from wtforms.validators import DataRequired
 from flask_babel import lazy_gettext as _l
 from helpers import binary_search
+from .db import *
 
 class LoginForm(FlaskForm):
     user = StringField(_l('Username'), validators = [DataRequired()])
@@ -29,9 +30,9 @@ class CheckinManager:
     def __init__(self, clubhouse=None):
         self.check_in_form = CheckinForm()
         if clubhouse:
-            self.clubhouse = clubhouse
-            # TODO: load actual lists from database and sort them alphabetically
-        if not clubhouse:
+            self.clubhouse = clubhouse # clubhouse is id number
+            # TODO: load actual lists from database
+        if not clubhouse: # testing purposes
             # NOTE: translation not needed here because names are displayed
             self.members_in = [("abc","manager signed-in 1"), ("bcd","manager signed-in 2")]
             self.members_out = [("ab","manager signed-out 3"),("bc","manager signed-out 4")]
@@ -49,14 +50,15 @@ class CheckinManager:
         self.check_in_form.check_in_id.choices = self.members_out
         self.check_in_form.check_out_id.choices = self.members_in
 
-    # TODO: database update, either here or in routes.py
-
     # check in member id_num
     # move from out list to in list
     def checkin_member(self, id_num):
         # extract member to be removed
         member = (id_num, self.id_to_name[id_num])
         self.members_out.remove(member)
+        # check-in to database
+        # TODO: check syntax
+        add_checkin()
         # insert member into sorted members_in list
         self.members_in.insert(binary_search(self.members_in, self.id_to_name[id_num]), member)
         self.setfields() # update visual
@@ -67,5 +69,6 @@ class CheckinManager:
         # extract member to be removed
         member = (id_num, self.id_to_name[id_num])
         self.members_in.remove(member)
+        add_checkout()
         self.members_out.insert(binary_search(self.members_out, self.id_to_name[id_num]), member)
         self.setfields()
